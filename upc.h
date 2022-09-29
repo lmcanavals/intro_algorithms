7fcf5cdabdcac49a7b29f50709213b7146fceaa5
Luis M Canaval S <43156214+lmcanavals@users.noreply.github.com>
2022-09-29 12:16:00 -0500

    try these 2 on *nix or macos

--- upc.h
+++ upc.h
@@ -1,37 +1,179 @@
-#pragma once
+/* #ifndef es la forma estandard de declarar un h, #pragma no funciona en gcc */
+#ifndef __UPC_H__
+#define __UPC_H__
 
+/* includes generales */
 #include <iostream>
 #include <string>
 
-using namespace std;
+#include <stdio.h>
 
-bool isInt(string str) {
+
+/*************************************
+ * validación
+ * **********************************/
+
+/* validar si string es un entero [+\-]?\d+ */
+bool isInt(std::string str) {
+	bool digits = false;
 	for (int i = 0; i < str.size(); ++i) {
-		if (str[i] < '0' || str[i] > '9') {
+		if (i == 0 && str[i] == '-' || str[i] == '+') {
+			continue;
+		} else if (str[i] >= '0' && str[i] <= '9') {
+			digits = true;
+		} else {
 			return false;
 		}
 	}
-	return str.size() > 0;
+	return digits;
 }
 
-int readInt(string msg) {
-	string str;
+/* validar si std::string es float forma simple [+\-]?\d+\.\d+ */
+bool isFloat(std::string str) {
+	bool digits = false;
+	bool dot = false;
+	for (int i = 0; i < str.size(); ++i) {
+		if (i == 0 && str[i] == '-' || str[i] == '+') {
+			continue;
+		} else if (str[i] >= '0' && str[i] <= '9') {
+			digits = true;
+		} else if (str[i] == '.') {
+			if (dot || !digits) {
+				return false;
+			} else {
+				dot = true;
+				digits = false;
+			}
+		}
+	}
+	return digits;
+}
+
+/* validación no muy elegante de entero */
+int readInt(std::string msg) {
+	std::string str;
 	do {
-		cout << msg; getline(cin, str);
+		std::cout << msg; getline(std::cin, str);
 		if (!isInt(str)) {
-			cout << "<" << str << "> no es un número entero.\n";
+			std::cout << "<" << str << "> no es un número entero.\n";
 		}
 	} while (!isInt(str));
 	return stoi(str);
 }
 
-int readIntRange(string msg, int min, int max) {
+/* validación un poco fea de float */
+float readFloat(std::string msg) {
+	std::string str;
+	do {
+		std::cout << msg; getline(std::cin, str);
+		if (!isFloat(str)) {
+			std::cout << "<" << str << "> no es un número real.\n";
+		}
+	} while (!isFloat(str));
+	return stof(str);
+}
+
+/* validando feamente un rango entero */
+int readIntRange(std::string msg, int min, int max) {
 	int num;
 	do {
 		num = readInt(msg);
 		if (num < min || num > max) {
-			cout << "<" << num << "> no está en el rango [" << min << ".." << max << "]\n";
+			std::cout << "<" << num << "> no está en el rango ["
+				<< min << ".." << max << "]\n";
 		}
 	} while (num < min || num > max);
 	return num;
 }
+
+/* validación de rango real */
+float readFloatRange(std::string msg, float min, float max) {
+	float num;
+	do {
+		num = readFloat(msg);
+		if (num < min || num > max) {
+			std::cout << "<" << num << "> no está en el rango ["
+				<< min << ".." << max << "]\n";
+		}
+	} while (num < min || num > max);
+	return num;
+}
+
+/*************************************
+ * elementos condicionales al sistema operativo
+ * **********************************/
+
+#ifdef _WIN32
+
+/* si se compila en windows, intenta usar windows.h */
+#include <windows.h>
+#include <conio.h>
+
+/* dormir (detener) proceso por x milisegundos */
+void sleep4(int milliseconds) {
+	Sleep(milliseconds);
+}
+
+/* limpiar pantalla */
+void clear() {
+	system("cls");
+}
+
+/* saltar a una posición x, y en la pantalla */
+void gotoxy(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),  coord);
+}
+
+/* else del bloque _WIN32 */
+#else
+
+/* para gcc, macos, *nix se usa en su lugar, ncurses y unistd */
+#include <unistd.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+
+/* dormir (detener) proceso por x milisegundos */
+void sleep4(int milliseconds) {
+	usleep(milliseconds * 1000);
+}
+
+/* limpiar pantalla */
+void clear() {
+	system("clear");
+}
+
+/* saltar a una posición x, y en la pantalla */
+void gotoxy(int x, int y) {
+	printf("%c[%d;%df", 0x1B, y, x);
+}
+
+/* detección de tecla presionada para linux
+ * gracias a Morgan Mattews
+ * https://www.flipcode.com/archives/_kbhit_for_Linux.shtml */
+int _kbhit() {
+	static const int STDIN = 0;
+	static bool initialized = false;
+
+	if (! initialized) {
+		// Use termios to turn off line buffering
+		termios term;
+		tcgetattr(STDIN, &term);
+		term.c_lflag &= ~ICANON;
+		tcsetattr(STDIN, TCSANOW, &term);
+		setbuf(stdin, NULL);
+		initialized = true;
+	}
+
+	int bytesWaiting;
+	ioctl(STDIN, FIONREAD, &bytesWaiting);
+	return bytesWaiting;
+}
+
+/* endif del bloque _WIN32 (else) */
+#endif
+
+#endif
---
+++ upctest.cpp
@@ -0,0 +1,37 @@
+#include "upc.h"
+
+/* no hay necesidad de usar windows.h ni conio.h, son incluidas en upc.h */
+
+using namespace std;
+
+int main() {
+
+	const int left = 10;
+	const int right = 39;
+	const int top = 5;
+	const int bottom = 19;
+
+	int x = left;
+	int y = top;
+	int dx = 1;
+	int dy = 1;
+
+	for (;;) {
+		clear();
+		gotoxy(x, y);
+		cout << "hello, world!" << endl;
+		x += dx; y += dy;
+		if (x < left || x > right) dx *= -1;
+		if (y < top || y > bottom) dy *= -1;
+
+		if (_kbhit()) {
+			char x = getchar();
+
+			cout << x << x << endl;
+			break;
+		}
+		sleep4(100);
+	}
+
+	return 0;
+}
