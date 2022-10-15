1718a3ec8a629b1cba7d4ad3ecc6a8f4aef1cb39
unknown <Alumnos@SB709-SERV.upc.edu.pe>
2022-10-15 14:23:01 -0500

    graficar

--- upc.h
+++ upc.h
@@ -1,183 +1,116 @@
-/* #ifndef es la forma estandard de declarar un h, #pragma no funciona en gcc */
-#ifndef __UPC_H__
-#define __UPC_H__
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
 
-/* includes generales */
-#include <iostream>
-#include <string>
+#include "upc.h"
 
-#include <stdio.h>
+using namespace std;
 
-
-/*************************************
- * validación
- * **********************************/
-
-/* validar si string es un entero [+\-]?\d+ */
-bool isInt(std::string str) {
-	bool digits = false;
-	for (int i = 0; i < str.size(); ++i) {
-		if (i == 0 && str[i] == '-' || str[i] == '+') {
-			continue;
-		} else if (str[i] >= '0' && str[i] <= '9') {
-			digits = true;
-		} else {
-			return false;
-		}
-	}
-	return digits;
+void random4(unsigned int* a, unsigned int* b, unsigned int* c, unsigned int* d) {
+    *a = (rand() % 40) + 1;
+    do {
+        *b = (rand() % 40) + 1;
+    } while (*b == *a);
+    do {
+        *c = (rand() % 40) + 1;
+    } while (*c == *a || *c == *b);
+    do {
+        *d = (rand() % 40) + 1;
+    } while (*d == *a || *d == *b || *d == *c);
 }
 
-/* validar si std::string es float forma simple [+\-]?\d+\.\d+ */
-bool isFloat(std::string str) {
-	bool digits = false;
-	bool dot = false;
-	for (int i = 0; i < str.size(); ++i) {
-		if (i == 0 && str[i] == '-' || str[i] == '+') {
-			continue;
-		} else if (str[i] >= '0' && str[i] <= '9') {
-			digits = true;
-		} else if (str[i] == '.') {
-			if (dot || !digits) {
-				return false;
-			} else {
-				dot = true;
-				digits = false;
-			}
-		}
-	}
-	return digits;
+unsigned int max4(unsigned int* a, unsigned int* b, unsigned int* c, unsigned int* d) {
+    if (*a > *b && *a > *c && *a > *d) {
+        return *a;
+    }
+    if (*b > *c && *b > *d) {
+        return *b;
+    }
+    if (*c > *d) {
+        return *c;
+    }
+    return *d;
 }
 
-/* validación no muy elegante de entero */
-int readInt(std::string msg) {
-	std::string str;
-	do {
-		std::cout << msg; getline(std::cin, str);
-		if (!isInt(str)) {
-			std::cout << "<" << str << "> no es un número entero.\n";
-		}
-	} while (!isInt(str));
-	return stoi(str);
+unsigned int min4(unsigned int* a, unsigned int* b, unsigned int* c, unsigned int* d) {
+    if (*a < *b && *a < *c && *a < *d) {
+        return *a;
+    }
+    if (*b < *c && *b < *d) {
+        return *b;
+    }
+    if (*c < *d) {
+        return *c;
+    }
+    return *d;
 }
 
-/* validación un poco fea de float */
-float readFloat(std::string msg) {
-	std::string str;
-	do {
-		std::cout << msg; getline(std::cin, str);
-		if (!isFloat(str)) {
-			std::cout << "<" << str << "> no es un número real.\n";
-		}
-	} while (!isFloat(str));
-	return stof(str);
+unsigned int maxOf3(unsigned int* a, unsigned int* b, unsigned int* c) {
+    if (*a > *b && *a > *c) {
+        return *a;
+    }
+    if (*b > *c) {
+        return *b;
+    }
+    return *c;
 }
 
-/* validando feamente un rango entero */
-int readIntRange(std::string msg, int min, int max) {
-	int num;
-	do {
-		num = readInt(msg);
-		if (num < min || num > max) {
-			std::cout << "<" << num << "> no está en el rango ["
-				<< min << ".." << max << "]\n";
-		}
-	} while (num < min || num > max);
-	return num;
+unsigned int secondMax(unsigned int* a, unsigned int* b, unsigned int* c, unsigned int* d) {
+    unsigned int x = max4(a, b, c, d);
+    if (*a == x) {
+        return maxOf3(b, c, d);
+    }
+    if (*b == x) {
+        return maxOf3(a, c, d);
+    }
+    if (*c == x) {
+        return maxOf3(a, b, d);
+    }
+    return maxOf3(a, b, c);
 }
 
-/* validación de rango real */
-float readFloatRange(std::string msg, float min, float max) {
-	float num;
-	do {
-		num = readFloat(msg);
-		if (num < min || num > max) {
-			std::cout << "<" << num << "> no está en el rango ["
-				<< min << ".." << max << "]\n";
-		}
-	} while (num < min || num > max);
-	return num;
+void stars(int x, int y, int n) {
+    for (int i = 0; i < n; ++i) {
+        gotoxy(x - i, y);
+        cout << "*";
+    }
 }
 
-/*************************************
- * elementos condicionales al sistema operativo
- * **********************************/
-
-#ifdef _WIN32
-
-/* si se compila en windows, intenta usar windows.h */
-#include <windows.h>
-#include <conio.h>
-
-/* dormir (detener) proceso por x milisegundos */
-void sleep4(int milliseconds) {
-	Sleep(milliseconds);
+void graph(unsigned int* a, unsigned int* b, unsigned int* c, unsigned int* d) {
+    stars(79, 2, *a); gotoxy(79 - *a - 1, 2); cout << setw(2) << *a;
+    stars(79, 3, *b); gotoxy(79 - *b - 1, 3); cout << setw(2) << *b;
+    stars(79, 4, *c); gotoxy(79 - *c - 1, 4); cout << setw(2) << *c;
+    stars(79, 5, *d); gotoxy(79 - *d - 1, 5); cout << setw(2) << *d;
 }
 
-/* limpiar pantalla */
-void clear() {
-	system("cls");
+int main() {
+    srand(time(nullptr));
+
+    char* op = new char;
+    unsigned int* a = new unsigned int;
+    unsigned int* b = new unsigned int;
+    unsigned int* c = new unsigned int;
+    unsigned int* d = new unsigned int;
+
+    do {
+        random4(a, b, c, d);
+
+        clear();
+        graph(a, b, c, d);
+        cout << endl;
+        cout << *a << " " << *b << " " << *c << " " << *d << endl;
+        cout << max4(a, b, c, d) << endl;
+        cout << secondMax(a, b, c, d) << endl;
+
+        *op = _getch();
+    } while (*op != 'x');
+
+    delete op;
+    delete a;
+    delete b;
+    delete c;
+    delete d;
+
+
+    return 0;
 }
-
-/* saltar a una posición x, y en la pantalla */
-void gotoxy(int x, int y) {
-	COORD coord;
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),  coord);
-}
-
-/* else del bloque _WIN32 */
-#else
-
-/* para gcc, macos, *nix se usa en su lugar, ncurses y unistd */
-#include <unistd.h>
-#include <sys/select.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-
-/* dormir (detener) proceso por x milisegundos */
-void sleep4(int milliseconds) {
-	usleep(milliseconds * 1000);
-}
-
-/* limpiar pantalla */
-void clear() {
-	system("clear");
-}
-
-/* saltar a una posición x, y en la pantalla */
-void gotoxy(int x, int y) {
-	printf("%c[%d;%df", 0x1B, y, x);
-}
-
-/* detección de tecla presionada para linux
- * gracias a Morgan Mattews
- * https://www.flipcode.com/archives/_kbhit_for_Linux.shtml */
-int _kbhit() {
-	static const int STDIN = 0;
-	static bool initialized = false;
-
-	if (! initialized) {
-		// Use termios to turn off line buffering
-		termios term;
-		tcgetattr(STDIN, &term);
-		term.c_lflag &= ~ICANON;
-		tcsetattr(STDIN, TCSANOW, &term);
-		setbuf(stdin, NULL);
-		initialized = true;
-	}
-
-	int bytesWaiting;
-	ioctl(STDIN, FIONREAD, &bytesWaiting);
-	return bytesWaiting;
-}
-
-int _getch() {
-	return getchar();
-}
-
-/* endif del bloque _WIN32 (else) */
-#endif
-
-#endif
